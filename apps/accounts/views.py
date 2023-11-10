6efb196f897cb2368f1db810ab4122bc4c8b946f
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-09 23:55:10 -0300

    create user api

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.db import transaction
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request, *args, **kwargs):
+        with transaction.atomic():
+            return super().create(request, *args, **kwargs)
