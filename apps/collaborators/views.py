b3cc86a45606981dda95ffb0264d12f1bfb69a63
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-16 23:42:47 -0300

    custom ListModelMixin

--- apps/collaborators/views.py
+++ apps/collaborators/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets, permissions
-from rest_framework.response import Response
+from rest_framework import permissions
 from .serializers import CollaboratorsSerializer
 from .models import Collaborator
-from .permissions import ReadOnlyPermission
+from apps.utils.permissions import ReadOnlyPermission
+from ..utils.mixins import CustomModelViewSet
 
 
-class CollaboratorsViewSet(viewsets.ModelViewSet):
+class CollaboratorsViewSet(CustomModelViewSet):
     queryset = Collaborator.objects.all()
     serializer_class = CollaboratorsSerializer
 
@@ -21,21 +21,3 @@ class CollaboratorsViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [ReadOnlyPermission]
         return [permission() for permission in permission_classes]
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-
-        total = queryset.count()
-        total_page = len(serializer.data)
-        return Response({
-            "total": total,
-            "total_page": total_page,
-            "data": serializer.data
-        })
