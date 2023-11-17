151be5e0f34e9f76cc6ae43bb5b5f8fde1a224bb
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-16 23:28:56 -0300

    define area restrita e adiciona total na response do list

--- apps/collaborators/serializers.py
+++ apps/collaborators/serializers.py
@@ -5,4 +5,16 @@ from .models import Collaborator
 class CollaboratorsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collaborator
-        exclude = ("active",)
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        if (self.context["request"].method == "GET" and
+                "list" in self.context["request"].resolver_match.url_name):
+            data["total"] = Collaborator.objects.count()
+
+            page_size = self.context["request"].query_params.get("page_size", 10)
+            data["total_page"] = (data["total"] + int(page_size) - 1) // int(page_size)
+
+        return data
--- apps/collaborators/views.py
+++ apps/collaborators/views.py
@@ -1,9 +1,41 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from .serializers import CollaboratorsSerializer
 from .models import Collaborator
+from .permissions import ReadOnlyPermission
 
 
 class CollaboratorsViewSet(viewsets.ModelViewSet):
-    queryset = Collaborator.objects.filter(active=True)
+    queryset = Collaborator.objects.all()
     serializer_class = CollaboratorsSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.validated:
+            return Collaborator.objects.all()
+        return Collaborator.objects.filter(active=True)
+
+    def get_permissions(self):
+        if self.request.user.validated:
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [ReadOnlyPermission]
+        return [permission() for permission in permission_classes]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        total = queryset.count()
+        total_page = len(serializer.data)
+        return Response({
+            "total": total,
+            "total_page": total_page,
+            "data": serializer.data
+        })
