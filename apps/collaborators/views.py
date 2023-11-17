211e77a55b18e17cca34429df9a612773ebc3137
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-16 23:51:13 -0300

    remove import unusable

--- apps/collaborators/views.py
+++ apps/collaborators/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, permissions
-from rest_framework.response import Response
 from .serializers import CollaboratorsSerializer
 from .models import Collaborator
 from apps.utils.permissions import ReadOnlyPermission
