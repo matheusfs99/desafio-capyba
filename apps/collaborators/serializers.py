b82e515af86a2a16f042b0ed5d802b9740ab920c
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-17 00:30:32 -0300

    fix total and total_page in data

--- apps/collaborators/serializers.py
+++ apps/collaborators/serializers.py
@@ -6,15 +6,3 @@ class CollaboratorsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collaborator
         fields = "__all__"
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-
-        if (self.context["request"].method == "GET" and
-                "list" in self.context["request"].resolver_match.url_name):
-            data["total"] = Collaborator.objects.count()
-
-            page_size = self.context["request"].query_params.get("page_size", 10)
-            data["total_page"] = (data["total"] + int(page_size) - 1) // int(page_size)
-
-        return data
