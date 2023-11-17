c4237d768db9104edd4340abb12e1d180e0382c7
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-17 00:11:53 -0300

    add pagination filters

--- apps/utils/pagination.py
+++ apps/utils/pagination.py
@@ -1,8 +1,14 @@
 from rest_framework import pagination
+from rest_framework.pagination import _positive_int
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 
 class CustomPagination(pagination.PageNumberPagination):
+    default_limit = api_settings.PAGE_SIZE
+    limit_query_param = "page_size"
+    max_limit = None
+
     def get_paginated_response(self, data):
         return Response({
             "links": {
@@ -13,3 +19,16 @@ class CustomPagination(pagination.PageNumberPagination):
             "count_in_page": len(data),
             "results": data
         })
+
+    def get_page_size(self, request):
+        if self.limit_query_param:
+            try:
+                return _positive_int(
+                    request.query_params[self.limit_query_param],
+                    strict=True,
+                    cutoff=self.max_limit
+                )
+            except (KeyError, ValueError):
+                pass
+
+        return self.default_limit
