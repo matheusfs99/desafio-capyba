ba52d36767a198f0850efa188ffc928733a35457
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-20 22:57:38 -0300

    set values from fields in collaborator factory

--- tests/factories/collaborators.py
+++ tests/factories/collaborators.py
@@ -3,5 +3,11 @@ from apps.collaborators.models import Collaborator
 
 
 class CollaboratorFactory(factory.Factory):
+    name = "Test"
+    age = 20
+    gender = "male"
+    role = "junior"
+    salary = 2000
+
     class Meta:
         model = Collaborator
