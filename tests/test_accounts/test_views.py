faaff373fd78761614a4ced091c494cba86d14ed
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2023-11-20 23:30:21 -0300

    add more tests

--- tests/test_accounts/test_views.py
+++ tests/test_accounts/test_views.py
@@ -1,10 +1,26 @@
+import json
 import pytest
+from pytest_factoryboy.fixture import register
+from http import HTTPStatus
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.reverse import reverse
 from apps.accounts.models import User
+from tests.factories.accounts import UserFactory
 
 pytestmark = pytest.mark.django_db
+
+register(UserFactory, "another_user")
+
+
+def test_list_users(api_client, user_factory, another_user):
+    user_factory().save()
+    another_user.email = "new_test@email.com"
+    another_user.save()
+    url = reverse("user-list")
+    response = api_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()["results"]) == 2
 
 
 def test_create_user(api_client, user_payload):
@@ -13,6 +29,43 @@ def test_create_user(api_client, user_payload):
 
     assert response.status_code == status.HTTP_201_CREATED
     assert User.objects.filter(email=user_payload["email"]).exists()
+
+
+@pytest.mark.parametrize(
+    "field", ["username", "email", "password"]
+)
+def test_create_user_without_field(field, api_client, user_payload):
+    user_payload.pop("photo")
+    user_payload.pop(field)
+    url = reverse("user-list")
+    response = api_client.post(url, user_payload)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_detail_user(api_client, user):
+    user.save()
+    url = reverse("user-detail", kwargs={"pk": user.id})
+    response = api_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_detail_user_nonexistent(api_client):
+    url = reverse("user-detail", kwargs={"pk": 123})
+    response = api_client.get(url)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_update_user(api_client, user):
+    user.save()
+    url = reverse("user-detail", kwargs={"pk": user.id})
+    payload = {
+        "username": "matheus"
+    }
+    response = api_client.patch(url, json.dumps(payload), content_type="application/json")
+
+    assert response.status_code == HTTPStatus.OK
+    assert user.id == response.json()["id"]
+    assert user.username != response.json()["username"]
 
 
 def test_login(api_client):
